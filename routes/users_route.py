53ce95a7a39c6f268c8663ce3c91b61f9d79bed3
fortune1237 <okolofortune4@gmail.com>
2024-04-12 12:38:46 +0100

    user authentication

--- routes/users_route.py
+++ routes/users_route.py
@@ -1,3 +1,45 @@
 from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException, status, Query
+from sqlmodel import Session, select
+from models.user_model import UserCreate, User, UserLogin, Token, UserOutput
+from typing import Annotated, Any
+from fastapi.security import OAuth2PasswordRequestForm
+from utils import get_password_hash, verify_password, create_access_token
+from datetime import timedelta
+from config import settings
 
 router = APIRouter()
+
+
+@router.post("/register", tags=["login"])
+async def register(
+    user: UserCreate, 
+    db: Annotated[Session, Depends(get_db)]
+):
+    #hash the password
+    user.password = get_password_hash(user.password)
+    new_user = User(**user.dict())
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return user
+
+
+@router.post("/login", tags=["login"])
+async def login(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()], 
+    db: Annotated[Session, Depends(get_db)]
+):
+    # get user by email
+    user = authenticate(
+        session=db, email=form_data.username, password=form_data.password
+    )
+    if not user:
+        raise HTTPException(status_code=400, detail="Incorrect email or password")
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    return Token(
+        access_token=create_access_token(
+            user.id, expires_delta=access_token_expires
+        )
+    )
+    
